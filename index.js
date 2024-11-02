3d881e9d436670792903dd3f273829f99de8778b
About-Computers <aabakks2012@gmail.com>
2024-11-02 16:01:49 +0530

    U2

---
+++ index.js
@@ -0,0 +1,10 @@
+function CTTL() {
+    document.getElementById("Home").style.backgroundColor = "#E0E0E0";
+    document.getElementById("themebtn").style.color = "#202020";
+    document.getElementById("themebtn2").style.color = "#202020";
+}
+function CTTD() {
+    document.getElementById("Home").style.backgroundColor = "#202020";
+    document.getElementById("themebtn").style.color = "#E0E0E0";
+    document.getElementById("themebtn2").style.color = "#E0E0E0";
+}
